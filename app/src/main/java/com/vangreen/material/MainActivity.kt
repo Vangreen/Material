9b539fad72b2402dfbd6d0792b413e9017567ed0
Paweł Wangryn <pwangryn@op.pl>
2018-10-17 09:33:39 +0200

    Add plus icon with toast

--- app/src/main/java/com/vangreen/material/MainActivity.kt
+++ app/src/main/java/com/vangreen/material/MainActivity.kt
@@ -3,10 +3,25 @@ package com.vangreen.material
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.bottom_app_bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == R.id.action_show_toast)
+            Toast.makeText(this, "Toast", Toast.LENGTH_SHORT).show()
+        return true
     }
 }
